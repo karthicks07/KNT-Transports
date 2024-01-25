8aeaa473cb7a102b7d3f050dff447e93ff14c368
Karthick_S <karthick712004@gmail.com>
2024-01-25 22:33:23 +0530

    Commit

---
+++ src/components/FunctionalComponents/Products.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+const Products=()=>
+{
+    return(
+        <div style={{height:"583px"}}>
+            <h1>This is product page</h1>
+        </div>
+    )
+}
+
+export default Products
