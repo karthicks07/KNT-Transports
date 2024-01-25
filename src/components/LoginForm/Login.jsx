3cdc46c5d87c4e25b5a3089b876cca98d210f4a3
Karthick_S <karthick712004@gmail.com>
2024-01-25 12:14:30 +0530

    Commit

--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -2,38 +2,48 @@ import React from "react";
 import './Login.css'
 // ...
 
-function Login(){
-      return (
-        <div className="page-container">
-          <div className="form1">
-            <h1>Registration Form</h1>
-            <div className="form2">
-                <center><div className="logintext">Login</div></center>
-                <form action="post">
-                  <table>
-                    <tr>
-                      <td><input type="text" placeholder="Name" /></td>
-                    </tr>
-                    <tr>
-                      <td><input type="email" placeholder="Email" /></td>
-                    </tr>
-                    <tr>
-                      <td><input type="password" placeholder="Password" /></td>
-                    </tr>
-                  </table>
-                  <div className="btns">
-                    <button className="signupbtn">Signup</button>
-                    <button className="loginbtn">Login</button>
-                  </div>
-                  <div className="forget">
-                    <a href="#">forgot password?</a></div>
-                </form>
-            </div>
+class Login extends React.Component{
+  changeState=()=>
+  {
+    console.log("Hi")
+  }
+  render() {
+    return (
+      <div className="page-container">
+        <div className="form1">
+          <h1>Registration Form</h1>
+          <div className="form2">
+            <center>
+              <div className="logintext">Login</div>
+            </center>
+            <form>
+              <table>
+                <tbody>
+                  <tr>
+                    <td><input type="text" placeholder="Name" /></td>
+                  </tr>
+                  <tr>
+                    <td><input type="email" placeholder="Email" /></td>
+                  </tr>
+                  <tr>
+                    <td><input type="password" placeholder="Password" /></td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="btns">
+                <button className="signupbtn">Signup</button>
+                <button className="loginbtn">Login</button>
+              </div>
+              <div className="forget">
+                <a href="" onClick={this.changeState}>forgot password?</a>
+              </div>
+            </form>
           </div>
         </div>
-      )
-    }
+      </div>
+    );
+  }
+}
 
-
-  export default Login
+export default Login
   
