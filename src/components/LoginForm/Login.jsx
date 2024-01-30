11845dcb31b27fff0bd3c626ec198ba9d3b12e26
Karthick_S <karthick712004@gmail.com>
2024-01-30 15:10:15 +0530

    Commit

--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -1,44 +1,62 @@
 import React from "react";
 import './Login.css'
 import { Link } from "react-router-dom";
-import Signup from "./Signup";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 // ...
 
-const Login =()=>{
-    return (
-      <div className="page-container">
-        <div className="form1">
-          <div className="form2">
+function Login() {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  let navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const res = await axios.post("http://localhost:3001/login",{email:email,password:password})
+    console.log(res.data)
+    if (res.data.loggedIn === true){
+        navigate("/")
+    }
+    setErrorMessage('Login up successful!');
+  }
+  return (
+    <div className="page-container">
+      <div className="form1">
+        <div className="form2">
           <h1 className="lh1">KNT Transports</h1>
           <div className="lin"></div>
-            <center>
-              <div className="logintext">Login</div>
-            </center>
-            <form>
-              <table>
-                <tbody>
-                  <tr>
-                    <td><input type="email" placeholder="Email" /></td>
-                  </tr>
-                  <tr>
-                    <td><input type="password" placeholder="Password" /></td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="btns">
-                <button className="btn1">Login</button>
-              </div>
-              <a href="#" className="forget-link">forget password?</a>
-              <div className="sign">
+          <center>
+            <div className="logintext">Login</div>
+          </center>
+          <form onSubmit={handleSubmit}>
+            <table>
+              <tbody>
+                <tr>
+                  <td><input type="email" placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}  required /></td>
+                </tr>
+                <tr>
+                  <td><input type="password" placeholder="Password"  id="password" value={password} onChange={(e) => setPassword(e.target.value)}  required /></td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="btns">
+              <button className="btn1">Login</button>
+              <p className="error-message">{errorMessage}</p>
+            </div>
+            <a href="#" className="forget-link">forget password?</a>
+            <div className="sign">
               <p className="account">Did't have an account?</p>
               <Link to="/signup" className="signuptext">Signup</Link>
-              </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Login
   
--- src/components/LoginForm/Signup.jsx
+++ src/components/LoginForm/Signup.jsx
@@ -1,58 +1,70 @@
 import React, { useState } from "react";
 import './Login.css'
 import { Link } from "react-router-dom";
-import Login from "./Login";
-import axios from "axios"
-// ...
+import axios from "axios";
 
-const Signup =()=>{
-  const[cname,updateCname]=useState("KNTT")
-  const updatecname=async(event)=>{
+function Signup() {
+  //   const [courseName,updateCourseName]=useState("KNT") 
+  //   const updateCourse = async(event) =>{
+  //     event.preventDefault()
+  //     const res=await axios.get("http://localhost:3001/signup")  
+  //     updateCourseName(res.data)   
+  //     console.log(res.data)
+  //   }
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async(event) => {
     event.preventDefault();
-    const res=await axios.get("https://localhost:3001/signup/")
-    updateCname(res.data)
-    console.log(res)
-  }
-    return (
-      <div className="page-container">
-        <div className="form1">
-          <div className="form22">
-            <h1 className="lh1">{cname?.cName} Transports</h1>
-            <div className="lin"></div>
-            <center>
-              <div className="logintext">Signup</div>
-            </center>
-            <form onSubmit={updatecname}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td><input type="text" placeholder="Name" /></td>
-                  </tr>
-                  <tr>
-                    <td><input type="email" placeholder="Email" /></td>
-                  </tr>
-                  <tr>
-                    <td><input type="number" placeholder="Phone (+91)" /></td>
-                  </tr>
-                  <tr>
-                    <td><input type="password" placeholder="Password" /></td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="btns">
-               <button className="btn1" type="submit">Signup</button>
-              </div>
-              <div className="signn">
+    const res = await axios.post("http://localhost:3001/signup", { name: name, email: email, phone: phone, password: password })
+    console.log(res.data)
+    setErrorMessage('Sign up successful!');
+  };
+
+  return (
+    <div className="page-container">
+      <div className="form1">
+        <div className="form22">
+          <h1 className="lh1">KNT Trasnsports</h1>
+          <div className="lin"></div>
+          <center>
+            <div className="logintext">Signup</div>
+          </center>
+          <form onSubmit={handleSubmit}>
+            <table>
+              <tbody>
+                <tr>
+                  <td><input type="text" placeholder="Name" id="name" value={name} onChange={(e) => setName(e.target.value)} required /></td>
+                </tr>
+                <tr>
+                  <td><input type="email" placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required /></td>
+                </tr>
+                <tr>
+                  <td><input type="number" placeholder="Phone (+91)" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required /></td>
+                </tr>
+                <tr>
+                  <td><input type="password" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required /></td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="btns">
+              <button className="btn1" type="submit">Signup</button>
+              <p className="error-message">{errorMessage}</p>
+            </div>
+            <div className="signn">
               <p className="account">Already have an account?</p>
               <Link to="/login" className="signuptext">Login</Link>
-              </div>
+            </div>
 
-            </form>
-          </div>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Signup
-  
